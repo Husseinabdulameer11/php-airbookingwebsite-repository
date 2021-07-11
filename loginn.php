f6ab5e095719c0d4b377db8d49aae525d98fec10
MR.Hussein Abdul-Ameer <Rivarelven@gmail.com>
2021-07-11 13:24:45 +0200

    Update loginn.php

--- loginn.php
+++ loginn.php
@@ -1,9 +1,8 @@
-<?php 
-$connection= new mysqli("localhost","root","","database_name");
-?>
+
 <html>
 <!--
 https://www.sitepoint.com/community/t/login-form-code/66257 //an example of a login system 
+https://codeshack.io/secure-login-system-php-mysql/ // another example of a login system
 -->
 <head></head>
 <body>
@@ -12,14 +11,6 @@ https://www.sitepoint.com/community/t/login-form-code/66257 //an example of a lo
 <input type="password"placeholder="password"name="password">
 <button type="submit" name="submit_btn">Submit</button>
 </form>
-<?php
-session_start();
-if(isset( $_SESSION['user_id'] ))
-{
- $message = 'Users is already logged in';
-}
-?>
-
 </body>
 </html>
 
