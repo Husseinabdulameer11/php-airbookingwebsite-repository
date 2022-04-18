f19ab534e9dd0ff27ffeef5dedfc9ff3eeb88328
MR.Hussein Abdul-Ameer <Rivarelven@gmail.com>
2022-04-18 03:30:07 +0200

    Update successtripchange.php

--- airplanebooking/successtripchange.php
+++ airplanebooking/successtripchange.php
@@ -12,8 +12,9 @@ $NewAmountOfSeats=$_SESSION["amount_of_seats"]-$_SESSION["SumOfBookers"];
 echo "<br>";
 echo"sum of bookers".$_SESSION["SumOfBookers"]."<br><br>";
 echo $NewAmountOfSeats;
+echo "<br>";
 echo $_SESSION["newbalance"];
-$sql="UPDATE `trips` SET `amount_of_seats`=".$_SESSION["NewAmountOfSeats"]."where";
+$sql="UPDATE `trips` SET `amount_of_seats`='$NewAmountOfSeats'Where `trip_id`=".$_SESSION["trip_ID"]."";
 //$sql="UPDATE `trips` SET `amount_of_seats`='$NewAmountOfSeats' WHERE `trip_name`=".$_SESSION['trip_name']."";
 $result=mysqli_query($connection,$sql);
 
